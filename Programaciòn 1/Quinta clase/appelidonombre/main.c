62ed6e23f2311f4ed219fb49e4c38a3d6bf47055
Diego <wchirdo@gmail.com>
2019-04-08 22:02:54 -0300

    Quinta clase

--- Programaciòn 1/Quinta clase/appelidonombre/main.c
+++ Programaciòn 1/Quinta clase/appelidonombre/main.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
 int main()
 {
     char nombre[20];
     char apellido[20];
     printf("Ingrese su nombre: ");
     scanf("%s", nombre);
-    nombre[0]=toupper(nombre[0]);
+    strlwr(nombre);  /* Pasa todo a minùscula */
+    nombre[0]=toupper(nombre[0]); /* Convierte el primer caracter a mayùscula */
     printf("Ingrese su apellido: ");
     scanf("%s", apellido);
+    strlwr(apellido);
     apellido[0]=toupper(apellido[0]);
-  /*  printf("\nSu nombre es %s y su apellido es %s", nombre, apellido); */
+   /*pellidoNombre = strcat(nombre, apellido);
+    apellidoNombre[0]='\0';
+    strcat(apellidoNombre, apellido);
+    strcat(apellidoNombre," ");
+    strcat(apellidoNombre, nombre);
+    printf("%s", apellidoNombre); */
     printf("\nSu nombre es %s", strcat(nombre, apellido));
+
     return 0;
 }
