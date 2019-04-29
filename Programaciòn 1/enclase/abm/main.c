97ee0a31dc449a77de5d97c6e6b06945f3b7df23
diego <wchirdo@gmail.com>
2019-04-29 20:46:33 -0300

    2904

---
+++ Programaciòn 1/enclase/abm/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "utn.h"
+
+int main()
+{
+    char nombre[20];
+    char apellido[20];
+    printf("Ingrese su nombre: ");
+    scanf("%s", nombre);
+    nombre[0]=mayuscula(nombre[0]);
+    printf("Ingrese su apellido: ");
+    scanf("%s", apellido);
+    strlwr(apellido);
+    apellido[0]=toupper(apellido[0]);
+   /*pellidoNombre = strcat(nombre, apellido);
+    apellidoNombre[0]='\0';
+    strcat(apellidoNombre, apellido);
+    strcat(apellidoNombre," ");
+    strcat(apellidoNombre, nombre);
+    printf("%s", apellidoNombre); */
+    printf("\nSu nombre es %s", strcat(nombre, apellido));
+
+    return 0;
+}
---
+++ Programaciòn 1/enclase/mayusculas.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int main()
+{
+    char nombre[20];
+    char apellido[20];
+    printf("Ingrese su nombre: ");
+    scanf("%s", nombre);
+    strlwr(nombre);  /* Pasa todo a minùscula */
+    nombre[0]=toupper(nombre[0]); /* Convierte el primer caracter a mayùscula */
+    printf("Ingrese su apellido: ");
+    scanf("%s", apellido);
+    strlwr(apellido);
+    apellido[0]=toupper(apellido[0]);
+   /*pellidoNombre = strcat(nombre, apellido);
+    apellidoNombre[0]='\0';
+    strcat(apellidoNombre, apellido);
+    strcat(apellidoNombre," ");
+    strcat(apellidoNombre, nombre);
+    printf("%s", apellidoNombre); */
+    printf("\nSu nombre es %s", strcat(nombre, apellido));
+
+    return 0;
+}
