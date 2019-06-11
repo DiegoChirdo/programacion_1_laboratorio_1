ebe2323b5508cea7f79fa8f00e506c6a9084dddb
diego <wchirdo@gmail.com>
2019-06-11 19:31:41 -0300

    zipzap

--- Programaciòn 1/TP2/funciones.c
+++ Programaciòn 1/TP2/funciones.c
@@ -29,8 +29,8 @@ int buscarLibre(EPersona vec[], int tam)
 int agregarPersona(EPersona vec[], int cant)
 {
     int libre;
-    char auxiliarEdad;
-    char auxiliarDni;
+    char auxiliarEdad[50];
+    char auxiliarDni[50];
     if(buscarLibre(vec, cant)==-1)
     {
         printf("\n No hay lugares libres. \n");
@@ -38,23 +38,24 @@ int agregarPersona(EPersona vec[], int cant)
     }
     libre=buscarLibre(vec, cant);
     printf("1- Agregar persona\n\n");
+    system("cls");
     if(!getStringLetras("Ingrese un nombre:\n", vec[libre].nombre))
     {
         printf("Solo puede estar compuesto por letras. \n");
         return 0;
     }
-    if(!getStringNumeros("Ingrese una edad: \n", atoi(auxiliarEdad)))
+    if(!getStringNumeros("Ingrese una edad: \n", auxiliarEdad))
     {
         printf("La edad solo puede estar compuesta por números. \n");
         return 0;
     }
-    if(!getStringNumeros("Ingrese un DNI: \n", atoi(auxiliarDni)))
+    if(!getStringNumeros("Ingrese un DNI: \n",auxiliarDni))
     {
         printf("El DNI solo puede estar compuesto por numeros. \n");
         return 0;
     }
-    vec[libre].edad=auxiliarEdad;
-    vec[libre].dni=auxiliarDni;
+    vec[libre].edad=atoi(auxiliarEdad);
+    vec[libre].dni=atoi(auxiliarDni);
     vec[libre].estado=-1;
     printf("HECHO!!!\n");
     return 0;
@@ -66,9 +67,11 @@ int borrarPersona(EPersona vec[], int cant)
     int i;
     if(vec[0].estado == 0)
     {
+        system("cls");
         printf("Aun no se han ingresado datos. \n");
         return 0;
     }
+    system("cls");
     printf("Ingrese el DNI de la persona a borrar: \n");
     scanf("%d", &dniBaja);
     for(i=0; i<cant; i++)
@@ -79,6 +82,7 @@ int borrarPersona(EPersona vec[], int cant)
             printf("HECHO!!!\n");
             return 0;
         }
+        system("cls");
         printf("No se encontro el DNI. \n");
         return 0;
     }
