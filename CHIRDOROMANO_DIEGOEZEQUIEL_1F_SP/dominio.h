9b4538f02a15bc32767a13505b57dcc4c409dc54
alumno <alumno>
2019-06-24 19:22:40 -0300

    hey

---
+++ CHIRDOROMANO_DIEGOEZEQUIEL_1F_SP/dominio.h
@@ -0,0 +1,13 @@
+#ifndef DOMINIO_H_INCLUDED
+#define DOMINIO_H_INCLUDED
+
+typedef struct
+{
+    int id;
+    char dominio[32];
+    int anio;
+    char tipo;
+}eDominio;
+
+
+#endif // DOMINIO_H_INCLUDED
